4fb1c9eba19390650c8d39821e0ea1b0e78fcc30
DC-VIETIS\TuanNQ <tuannq35@vietis.com.vn>
2022-11-23 16:51:14 +0700

    add footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  date: Date;
+  year: number;
+  alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  constructor() {
+    this.date = new Date();
+    this.year = this.date.getFullYear();
   }
 
+  ngOnInit(): void {}
 }
